689c23656d73fe8cba7dd910688c24bb697c1278
squirrel <squirrel@hengha.ren>
2017-06-14 22:50:39 +0800

    first commit

---
+++ Debug/src/linelist/OrderList.d
@@ -0,0 +1,4 @@
+src/linelist/OrderList.o: ../src/linelist/OrderList.c \
+  ../src/linelist/common/linelist.h
+
+../src/linelist/common/linelist.h:
---
+++ src/linelist/common/linelist.h
@@ -0,0 +1,6 @@
+#define MaxSize 50
+typedef int ElemType;
+typedef struct{
+	ElemType data[MaxSize];
+	int length;
+}SqList;
